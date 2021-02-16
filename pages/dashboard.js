08d8e01dd654fddd5d4b6612a8b79cd7ade1b4e3
Agustin Moya <agustin.moya@mercadolibre.com>
2021-02-16 16:54:58 -0300

    Firebase Admin with Next.js + SWR

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,14 +1,31 @@
-import { useAuth } from "@/lib/auth";
+import useSWR from "swr";
+
+import DashboardShell from "@/components/DashboardShell";
 import EmptyState from "@/components/EmptyState";
+import fetcher from "@/utils/fetcher";
+import SiteTableSkeleton from "@/components/SiteTableSkeleton";
+import { useAuth } from "@/lib/auth";
+import SiteTable from "@/components/SiteTable";
 
 const Dashboard = () => {
   const auth = useAuth();
+  const { data } = useSWR("/api/sites", fetcher);
 
-  if (!auth.user) {
-    return "Loading...";
+  console.log(data);
+
+  if (!data) {
+    return (
+      <DashboardShell>
+        <SiteTableSkeleton />
+      </DashboardShell>
+    );
   }
 
-  return <EmptyState />;
+  return (
+    <DashboardShell>
+      {data.sites ? <SiteTable sites={data.sites} /> : <EmptyState />}
+    </DashboardShell>
+  );
 };
 
 export default Dashboard;
