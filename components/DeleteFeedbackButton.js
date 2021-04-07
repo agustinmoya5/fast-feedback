3dd2cf2c626a9f9f18fdba7d17554063616ec3a9
Agustin Moya <agustin.moya@mercadolibre.com>
2021-04-06 23:04:51 -0300

    Fix delete icon

--- components/DeleteFeedbackButton.js
+++ components/DeleteFeedbackButton.js
@@ -11,6 +11,8 @@ import {
   Button,
 } from "@chakra-ui/react";
 
+import { DeleteIcon } from "@chakra-ui/icons";
+
 import { deleteFeedback } from "@/lib/db";
 import { useAuth } from "@/lib/auth";
 
@@ -40,7 +42,7 @@ const DeleteFeedbackButton = ({ feedbackId }) => {
     <>
       <IconButton
         aria-label="Delete feedback"
-        icon="delete"
+        icon={<DeleteIcon />}
         variant="ghost"
         onClick={() => setIsOpen(true)}
       />
