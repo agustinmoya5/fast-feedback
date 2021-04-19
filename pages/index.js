90a34c97e6c655546c6049458b06595ff98e5d15
Agustin Moya <agustin.moya@mercadolibre.com>
2021-04-19 20:35:25 -0300

    Set up stripe for subscriptions (#9)
    * Fix logins redirects + use site id
    
    * Add logging and handle exceptions in API routes
    
    * Set up stripe for subscription payments

--- pages/index.js
+++ pages/index.js
@@ -1,108 +1,128 @@
 import Head from "next/head";
-import { Box, Button, Flex, Text, Icon, Link, Stack } from "@chakra-ui/react";
+import { Box, Button, Flex, Text, Link, Stack } from "@chakra-ui/react";
 
 import { useAuth } from "@/lib/auth";
+import { getAllFeedback } from "@/lib/db-admin";
+import Feedback from "@/components/Feedback";
+import FeedbackLink from "@/components/FeedbackLink";
 
 import { FastFeedbackIcon, GithubIcon, GoogleIcon } from "@/public/icons";
 
-const Home = () => {
-  const { user, signinWithGithub, signinWithGoogle } = useAuth();
+const SITE_ID = "GBxxkac8bI4MuAP6fo8V";
+
+export async function getStaticProps(context) {
+  const { feedback } = await getAllFeedback(SITE_ID);
+
+  return {
+    props: {
+      allFeedback: feedback,
+    },
+    revalidate: 1,
+  };
+}
+
+const Home = ({ allFeedback }) => {
+  const { user, signinWithGoogle, signinWithGitHub } = useAuth();
 
   return (
-    <Box bg="gray.100">
-      <Flex
-        as="main"
-        direction="column"
-        align="center"
-        justify="center"
-        h="100vh"
-        maxW="400px"
-        margin="0 auto"
-      >
-        <Head>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
+    <>
+      <Box bg="gray.100" py={16}>
+        <Flex as="main" direction="column" maxW="700px" margin="0 auto">
+          <Head>
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
               if (document.cookie && document.cookie.includes('fast-feedback-auth')) {
                 window.location.href = "/dashboard"
               }
             `,
-            }}
-          />
-          <title>Fast Feedback</title>
-        </Head>
-        <FastFeedbackIcon color="black.500" boxSize="64px" mb={2} />
-        <Text mb={4} fontSize="lg" p={6}>
-          <Text as="span" fontWeight="bold" display="inline">
-            Fast Feedback
+              }}
+            />
+            <title>Fast Feedback</title>
+          </Head>
+          <FastFeedbackIcon color="black.500" boxSize="64px" mb={2} />
+          <Text mb={4} fontSize="lg" py={4}>
+            <Text as="span" fontWeight="bold" display="inline">
+              Fast Feedback
+            </Text>
+            {" is being built as part of "}
+            <Link
+              href="https://react2025.com"
+              isExternal
+              textDecoration="underline"
+            >
+              React 2025
+            </Link>
+            {`. It's the easiest way to add comments or reviews to your static site. It's still a work-in-progress, but you can try it out by logging in.`}
           </Text>
-          {" is being built as part of "}
-          <Link
-            href="https://react2025.com"
-            isExternal
-            textDecoration="underline"
-          >
-            React 2025
-          </Link>
-          {`. It's the easiest way to add comments or reviews to your static site. It's still a work-in-progress, but you can try it out by logging in.`}
-        </Text>
-        {user ? (
-          <Button
-            as="a"
-            href="/dashboard"
-            backgroundColor="white"
-            color="gray.900"
-            variant="outline"
-            fontWeight="medium"
-            mt={4}
-            size="lg"
-            _hover={{ bg: "gray.100" }}
-            _active={{
-              bg: "gray.100",
-              transform: "scale(0.95)",
-            }}
-          >
-            View Dashboard
-          </Button>
-        ) : (
-          <Stack>
+          {user ? (
             <Button
-              onClick={(e) => signinWithGithub()}
+              as="a"
+              href="/dashboard"
               backgroundColor="gray.900"
               color="white"
               fontWeight="medium"
-              leftIcon={<GithubIcon />}
               mt={4}
-              size="lg"
+              maxW="200px"
               _hover={{ bg: "gray.700" }}
               _active={{
                 bg: "gray.800",
                 transform: "scale(0.95)",
               }}
             >
-              Sign In with GitHub
+              View Dashboard
             </Button>
-            <Button
-              onClick={(e) => signinWithGoogle()}
-              backgroundColor="white"
-              color="gray.900"
-              variant="outline"
-              fontWeight="medium"
-              leftIcon={<GoogleIcon />}
-              mt={4}
-              size="lg"
-              _hover={{ bg: "gray.100" }}
-              _active={{
-                bg: "gray.100",
-                transform: "scale(0.95)",
-              }}
-            >
-              Sign In with Google
-            </Button>
-          </Stack>
-        )}
-      </Flex>
-    </Box>
+          ) : (
+            <Stack isInline>
+              <Button
+                onClick={(e) => signinWithGitHub()}
+                backgroundColor="gray.900"
+                color="white"
+                fontWeight="medium"
+                leftIcon={<GithubIcon />}
+                size="lg"
+                _hover={{ bg: "gray.700" }}
+                _active={{
+                  bg: "gray.800",
+                  transform: "scale(0.95)",
+                }}
+              >
+                Sign In with GitHub
+              </Button>
+              <Button
+                onClick={(e) => signinWithGoogle()}
+                backgroundColor="white"
+                color="gray.900"
+                variant="outline"
+                fontWeight="medium"
+                leftIcon={<GoogleIcon />}
+                size="lg"
+                _hover={{ bg: "gray.100" }}
+                _active={{
+                  bg: "gray.100",
+                  transform: "scale(0.95)",
+                }}
+              >
+                Sign In with Google
+              </Button>
+            </Stack>
+          )}
+        </Flex>
+      </Box>
+      <Box
+        display="flex"
+        flexDirection="column"
+        width="full"
+        maxWidth="700px"
+        margin="0 auto"
+        mt={8}
+      >
+        <FeedbackLink siteId={SITE_ID} />
+        {allFeedback.map((feedback) => (
+          <Feedback key={feedback.id} {...feedback} />
+        ))}
+      </Box>
+    </>
   );
 };
 
