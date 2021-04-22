7edd7f33f7348d622555eae187964d463bbf3e61
Agustin Moya <agustin.moya@mercadolibre.com>
2021-04-21 22:46:06 -0300

    A lot of changes (#10)

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { Global, css } from "@emotion/react";
 
+import { DefaultSeo } from "next-seo";
+
 import { AuthProvider } from "@/lib/auth";
 
 import customTheme from "@/styles/theme";
+
+import SEO from "../next-seo.config";
 
 const GlobalStyle = ({ children }) => {
   return (
@@ -30,6 +34,7 @@ const App = ({ Component, pageProps }) => {
   return (
     <ChakraProvider theme={customTheme}>
       <AuthProvider>
+        <DefaultSeo {...SEO} />
         <GlobalStyle />
         <Component {...pageProps} />
       </AuthProvider>
